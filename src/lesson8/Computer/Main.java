fb44739c2f702f05ec1e8a7cc73c0b9a3f1bbabb
MakushevEvgenii <evmakush@gmail.com>
2025-09-11 21:59:31 +0300

    задачки, интерфейсы, астрактные классы

--- src/lesson8/Computer/Main.java
+++ src/lesson8/Computer/Main.java
@@ -1,5 +1,10 @@
 package lesson8.Computer;
-
+/*Задача 2:
+Создать абстрактный класс Computer и абстрактный метод getClassName(). Создать
+классы Hp и Mac и отнаследоваться от Computer. Переопределить метод getClassName()
+в классах-наследниках так, чтобы они выводили сообщения «I’m HP» или «I’m Mac». В
+методе main класса Main создать объекты классов Hp Mac и вызвать методы для каждого
+из этих объектов*/
 public class Main {
     public static void main(String[] args) {
         HP hp = new HP();
