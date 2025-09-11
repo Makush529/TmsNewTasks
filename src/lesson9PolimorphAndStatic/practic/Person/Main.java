ef26ffada3d59ed7bf663e1a958951abf2ef2bb4
MakushevEvgenii <evmakush@gmail.com>
2025-09-11 15:07:57 +0300

    практические задачки

--- src/lesson9PolimorphAndStatic/practic/Person/Main.java
+++ src/lesson9PolimorphAndStatic/practic/Person/Main.java
@@ -1,5 +1,9 @@
 package lesson9PolimorphAndStatic.practic.Person;
 
+/*Задача 2:
+Создать класс Person. Написать статическое финальное поле name и статический метод,
+который выводит его имя в консоль. Реализовать в методе main другого класса вызов
+поля и метода не создавая объекта класса Person.*/
 public class Main {
     public static void main(String[] args) {
         System.out.println(Person.name);
--- src/lesson9PolimorphAndStatic/practic/Shape/Shape.java
+++ src/lesson9PolimorphAndStatic/practic/Shape/Shape.java
@@ -1,7 +1,12 @@
 package lesson9PolimorphAndStatic.practic.Shape;
 
+/*Задача 1:
+Реализовать иерархию, показанную ниже, переопределить методы draw и erase в каждом
+из классов так, чтобы он выводил “I draw ”+ имя класса и “I erase ” + имя класса. Показать
+работу каждого из методов в main другого класса*/
 abstract class Shape {
     public abstract void draw();
+
     public abstract void erase();
 }
 
