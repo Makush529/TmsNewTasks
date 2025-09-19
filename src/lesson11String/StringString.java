734dc9f45bce371bb6551a3cbe748b192d28930a
MakushevEvgenii <evmakush@gmail.com>
2025-09-19 22:47:16 +0300

    DZ 11

---
+++ src/lesson11String/StringString.java
@@ -0,0 +1,61 @@
+package lesson11String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Arrays.*;
+
+public class StringString {
+    public static void main(String[] args) {
+        /*1. Ввести 3 строки с консоли, найти самую короткую и самую длинную строки. Вывести
+        найденные строки и их длину.
+        2. Ввести 3 строки с консоли. Упорядочить и вывести строки в порядке возрастания
+        значений их длины.
+        3. Ввести 3 строки с консоли. Вывести на консоль те строки, длина которых меньше
+        средней, а также их длину.
+        4. Ввести 3 строки с консоли. Найти слово, состоящее только из различных символов.
+        Если таких слов несколько, найти первое из них.
+        5. Вывести на консоль новую строку, которой задублирована каждая буква из
+        начальной строки. Например, "Hello"-> "HHeelllloo".*/
+
+        Scanner scanner = new Scanner(System.in);
+        String string1 = scanner.nextLine();
+        String string2 = scanner.nextLine();
+        String string3 = scanner.nextLine();
+        scanner.close();
+
+
+        int stringShort = Integer.MAX_VALUE;
+        int stringLong = 0;
+        int indexStringShort = 0;
+        int indexStringLong = 0;
+
+        String[] strings = new String[3];
+        strings[0] = string1;
+        strings[1] = string2;
+        strings[2] = string3;
+
+        for (int i = 0; i<=2; i++){
+            if(stringShort>=strings[i].length()){
+                stringShort= strings[i].length();
+                indexStringShort=i;
+            }
+            if (stringLong<=strings[i].length()){
+                stringLong=strings[i].length();
+                indexStringLong=i;
+            }
+        }
+        System.out.println("1. Сравнение длинны: ");
+        System.out.println(strings[indexStringLong] + " символов: " + stringLong + "\n"+
+                strings[indexStringShort] + " символов: " + stringShort);
+
+        System.out.println("2. Вывод по возрастанию количества символов: ");
+        //Arrays.sort(strings);//не подойдет
+        //System.out.println(Arrays.toString(strings));
+
+
+
+
+
+    }
+}
