34c7bc414cc56d0a4d12d74bb53f63e79c446a1a
MakushevEvgenii <evmakush@gmail.com>
2025-10-08 22:23:53 +0300

    dz14

---
+++ src/lesson15List/Star/CustomArrayList.java
@@ -0,0 +1,11 @@
+package lesson15List.Star;
+
+public class CustomArrayList <T>{
+    private Object[]elements;
+    private int size;
+
+    public void increasingTheArray(){
+        int oldLength = elements.length;
+    };
+
+}
