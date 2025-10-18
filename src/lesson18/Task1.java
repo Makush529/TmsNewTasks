272f97ca503135c8ee77d58473ff1dfebbdd3b93
MakushevEvgenii <evmakush@gmail.com>
2025-10-18 14:24:59 +0300

    DZ 18

--- src/lesson18/Task1.java
+++ src/lesson18/Task1.java
@@ -1,4 +1,28 @@
 package lesson18;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.random;
+
 public class Task1 {
+    //    Задача 1:
+//    Создать коллекцию класса ArrayList наполнить ее элементами типа Integer. С помощью
+//    Stream'ов:
+//- Удалить дубликаты
+//- Оставить только четные элементы
+//- Вывести сумму оставшихся элементов в стриме
+    public static void main(String[] args) {
+        List<Integer> number = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            number.add((int) (Math.random() * 100) + 1);
+        }
+        System.out.println(number);
+        int sum=number.stream()
+                .distinct()
+                .filter(n->n%2==0)
+                .mapToInt(n->n.intValue())
+                .sum();
+        System.out.println(sum);
+    }
 }
